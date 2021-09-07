ef46fd586f2677b3574611e5e99f3ed435dc3343
ericaferrao <ferraoerica@gmail.com>
2021-09-07 17:53:07 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,23 @@
 import { Component, VERSION } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 //  import * as data from './data.json';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
   products: any = [];
 
-  constructor(private httpClient: HttpClient){
-    
-  }
-  ngOnInit(){
-    console.log("erica");
-    this.httpClient.get("assets/data.json").subscribe(data =>{
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit() {
+    console.log('erica');
+    this.httpClient.get('assets/data.json').subscribe(data => {
       console.log(data);
-      console.log("erica");
+      console.log('erica');
       this.products = data;
-    })
+    });
   }
 }
