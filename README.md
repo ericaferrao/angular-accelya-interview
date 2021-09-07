a623ef7b2e11fbc1026fa240eca655b2157d81be
ericaferrao <ferraoerica@gmail.com>
2021-09-07 17:45:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-js41bs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-js41bs)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, VERSION } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+//  import * as data from './data.json';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+
+  products: any = [];
+
+  constructor(private httpClient: HttpClient){
+    
+  }
+  ngOnInit(){
+    console.log("erica");
+    this.httpClient.get("assets/data.json").subscribe(data =>{
+      console.log(data);
+      console.log("erica");
+      this.products = data;
+    })
+  }
+}
