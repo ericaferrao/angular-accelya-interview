ffe202aef8da11f01eb7c212eea79070ac6787b3
ericaferrao <ferraoerica@gmail.com>
2021-09-08 00:02:07 +0530

    6 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ModalComponent } from './modal/modal.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ,HttpClientModule],
-  declarations: [ AppComponent, HelloComponent, ModalComponent ],
+  declarations: [ AppComponent, HelloComponent, ModalComponent, FooterComponent, HeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
